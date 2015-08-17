d1fa51a836b983d695d94e93a7c6d667fb9b932f
yiminghe <yiminghe@gmail.com>
2015-08-17 23:18:12 +0800

    Updates

--- examples/bootstrap.js
+++ examples/bootstrap.js
@@ -1,19 +1,21 @@
-webpackJsonp([0],[
-/* 0 */
+webpackJsonp([1],{
+
+/***/ 0:
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(1);
+	module.exports = __webpack_require__(31);
 
 
 /***/ },
-/* 1 */
+
+/***/ 31:
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 	
-	__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"rc-dialog/assets/index.css\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"rc-dialog/assets/bootstrap.css\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 	
 	var _react = __webpack_require__(2);
 	
@@ -114,5 +116,6 @@ webpackJsonp([0],[
 	), document.getElementById('__react-content'));
 
 /***/ }
-]);
-//# sourceMappingURL=ant-design.js.map
+
+});
+//# sourceMappingURL=bootstrap.js.map
