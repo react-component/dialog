fcf1ed889d3b15fc1c21722b392592f00ed447b2
yiminghe <yiminghe@gmail.com>
2015-08-17 23:12:59 +0800

    add tc

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -2,7 +2,7 @@
 
 var expect = require('expect.js');
 var Dialog = require('../index');
-require('/assets/bootstrap.css');
+require('../assets/bootstrap.css');
 var $ = require('jquery');
 var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
@@ -19,24 +19,20 @@ describe('dialog', function () {
   document.body.appendChild(container);
 
   var callback1;
-  var callback2;
 
   beforeEach(function () {
     function onClose() {
       callback1 = 1;
-    }
-
-    function onShow() {
-      callback2 = 1;
+      dialog.setState({
+        visible: false
+      });
     }
 
     callback1 = 0;
-    callback2 = 0;
     dialog = React.render((<Dialog
       style={{width: 600}}
       title={title}
-      onClose={onClose}
-      onShow={onShow}>
+      onClose={onClose}>
       <p>第一个dialog</p>
     </Dialog>), container);
   });
@@ -46,9 +42,10 @@ describe('dialog', function () {
   });
 
   it('show', function (done) {
-    dialog.show();
+    dialog.setState({
+      visible: true
+    });
     setTimeout(function () {
-      expect(callback2).to.be(1);
       expect($('.rc-dialog-wrap').hasClass('rc-dialog-wrap-hidden')).not.to.be.ok();
       done();
     }, 10);
@@ -56,8 +53,12 @@ describe('dialog', function () {
   });
 
   it('close', function (done) {
-    dialog.show();
-    dialog.close();
+    dialog.setState({
+      visible: true
+    });
+    dialog.setState({
+      visible: false
+    });
     setTimeout(function () {
       expect($('.rc-dialog-wrap').hasClass('rc-dialog-wrap-hidden')).to.be.ok();
       done();
@@ -69,14 +70,18 @@ describe('dialog', function () {
   });
 
   it('mask', function () {
-    dialog.show();
+    dialog.setState({
+      visible: true
+    });
     expect($('.rc-dialog-mask').length).to.be(1);
 
   });
 
   it('click close', function (done) {
     async.series([function (done) {
-      dialog.show();
+      dialog.setState({
+        visible: true
+      });
       setTimeout(done, 10);
     }, function (done) {
       var btn = $('.rc-dialog-header a')[0];
@@ -91,7 +96,9 @@ describe('dialog', function () {
 
   it('esc to close', function (done) {
     async.series([function (done) {
-      dialog.show();
+      dialog.setState({
+        visible: true
+      });
       setTimeout(done, 10);
     }, function (done) {
       Simulate.keyDown($('.rc-dialog')[0], {
@@ -105,36 +112,21 @@ describe('dialog', function () {
     }], done)
   });
 
-  describe('renderToBody', function () {
-    it('defaults to true', function () {
-      var d = React.render(<Dialog>
-        <p className="renderToBody">1</p>
-      </Dialog>, container);
-      expect($('.renderToBody').length).to.be(0);
-      expect($('.rc-dialog-container').length).to.be(0);
-      d.show();
-      expect($('.rc-dialog-container').length).to.be(1);
-      expect($('.renderToBody').length).to.be(1);
-      expect($('.rc-dialog-wrap')[0].parentNode).not.to.be(container);
-      expect($('.rc-dialog-wrap')[0].parentNode.className).to.be('rc-dialog-container');
-      React.unmountComponentAtNode(container);
-      expect($('.renderToBody').length).to.be(0);
-      expect($('.rc-dialog-container').length).to.be(0);
+  it('renderToBody', function () {
+    var d = React.render(<Dialog>
+      <p className="renderToBody">1</p>
+    </Dialog>, container);
+    expect($('.renderToBody').length).to.be(0);
+    expect($('.rc-dialog-container').length).to.be(0);
+    d.setState({
+      visible: true
     });
-
-    it('can be false', function () {
-      var d = React.render(<Dialog renderToBody={false}>
-        <p className="renderToBodyFalse">1</p>
-      </Dialog>, container);
-      expect($('.renderToBodyFalse').length).to.be(0);
-      expect($('.rc-dialog-container').length).to.be(0);
-      d.show();
-      expect($('.rc-dialog-container').length).to.be(0);
-      expect($('.renderToBodyFalse').length).to.be(1);
-      expect($('.rc-dialog-wrap')[0].parentNode).to.be(container);
-      React.unmountComponentAtNode(container);
-      expect($('.renderToBodyFalse').length).to.be(0);
-      expect($('.rc-dialog-container').length).to.be(0);
-    });
+    expect($('.rc-dialog-container').length).to.be(1);
+    expect($('.renderToBody').length).to.be(1);
+    expect($('.rc-dialog-wrap')[0].parentNode).not.to.be(container);
+    expect($('.rc-dialog-wrap')[0].parentNode.className).to.be('rc-dialog-container');
+    React.unmountComponentAtNode(container);
+    expect($('.renderToBody').length).to.be(0);
+    expect($('.rc-dialog-container').length).to.be(0);
   });
 });
