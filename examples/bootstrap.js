cf4a5168250fcb8f0a11523f302a485029da1382
yiminghe <yiminghe@gmail.com>
2015-08-17 23:17:52 +0800

    add bootstrap theme demo

---
+++ examples/bootstrap.js
@@ -0,0 +1,69 @@
+'use strict';
+
+import 'rc-dialog/assets/bootstrap.css';
+import React from 'react';
+import Dialog from 'rc-dialog';
+
+var MyControl = React.createClass({
+  getInitialState(){
+    return {
+      visible:false,
+      destroyOnClose:false,
+    };
+  },
+
+  onClick: function (e) {
+    this.setState({
+      mousePosition: {
+        x:e.pageX,
+        y:e.pageY
+      },
+      visible:true
+    });
+  },
+
+  onClose(){
+    this.setState({
+      visible:false
+    });
+  },
+
+  onDestroyOnCloseChange(e){
+    this.setState({
+      destroyOnClose:e.target.checked
+    });
+  },
+
+  render: function () {
+    var dialog;
+    if(this.state.visible || !this.state.destroyOnClose){
+      dialog= <Dialog visible={this.state.visible}
+              animation="zoom"
+              maskAnimation="fade"
+              onClose={this.onClose}
+              style={{width:600}}
+              mousePosition={this.state.mousePosition} title={<div> 第二个弹框</div>}>
+        <input />
+        <p>basic modal</p>
+      </Dialog>;
+   }
+    return (
+      <div>
+        <p>
+        <button className="btn btn-primary" onClick={this.onClick}>show dialog</button>
+          &nbsp;
+          <label>destroy on close: <input type="checkbox" checked={this.state.destroyOnClose} onChange={this.onDestroyOnCloseChange}/></label>
+          </p>
+        {dialog}
+      </div>
+    );
+  }
+});
+
+React.render(
+  <div>
+    <h2>ant-design dialog</h2>
+    <MyControl/>
+  </div>,
+  document.getElementById('__react-content')
+);
