cdfb34e4b1be6f44dd99ab7b52a3f2fd1a207091
RaoHai <surgesoft@gmail.com>
2016-05-17 15:14:31 +0800

    determine whether this.refs exisits

--- src/Dialog.jsx
+++ src/Dialog.jsx
@@ -102,7 +102,9 @@ const Dialog = React.createClass({
   },
 
   onAnimateLeave() {
-    this.refs.wrap.style.display = 'none';
+    if (this.refs.wrap) {
+      this.refs.wrap.style.display = 'none';
+    }
     this.props.onAfterClose();
   },
 
