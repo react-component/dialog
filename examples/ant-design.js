360a9045f4eda52dca93c48f5e72a3a612ee5e30
paranoidjk <hust2012jiangkai@gmail.com>
2017-06-01 17:48:31 +0800

    Updates

--- examples/ant-design.js
+++ examples/ant-design.js
@@ -1,124 +1,122 @@
-webpackJsonp([0],{
+webpackJsonp([1],{
 
-/***/ 0:
-/***/ (function(module, exports, __webpack_require__) {
+/***/ 132:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-	module.exports = __webpack_require__(1);
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck__ = __webpack_require__(40);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_createClass__ = __webpack_require__(41);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_createClass___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_createClass__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_possibleConstructorReturn__ = __webpack_require__(43);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_possibleConstructorReturn___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_possibleConstructorReturn__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_babel_runtime_helpers_inherits__ = __webpack_require__(42);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_babel_runtime_helpers_inherits___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_babel_runtime_helpers_inherits__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_rc_dialog_assets_index_less__ = __webpack_require__(185);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_rc_dialog_assets_index_less___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_rc_dialog_assets_index_less__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_react__ = __webpack_require__(13);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_react_dom__ = __webpack_require__(35);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_6_react_dom__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__src_DialogWrap__ = __webpack_require__(86);
 
 
-/***/ }),
 
-/***/ 1:
-/***/ (function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
-	var _classCallCheck2 = __webpack_require__(2);
-	
-	var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
-	
-	var _possibleConstructorReturn2 = __webpack_require__(3);
-	
-	var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
-	
-	var _inherits2 = __webpack_require__(72);
-	
-	var _inherits3 = _interopRequireDefault(_inherits2);
-	
-	__webpack_require__(80);
-	
-	var _react = __webpack_require__(81);
-	
-	var _react2 = _interopRequireDefault(_react);
-	
-	var _reactDom = __webpack_require__(116);
-	
-	var _reactDom2 = _interopRequireDefault(_reactDom);
-	
-	var _DialogWrap = __webpack_require__(262);
-	
-	var _DialogWrap2 = _interopRequireDefault(_DialogWrap);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	/* eslint no-console:0 */
-	var MyControl = function (_React$Component) {
-	    (0, _inherits3.default)(MyControl, _React$Component);
-	
-	    function MyControl() {
-	        (0, _classCallCheck3.default)(this, MyControl);
-	
-	        var _this = (0, _possibleConstructorReturn3.default)(this, _React$Component.apply(this, arguments));
-	
-	        _this.state = {
-	            visible: false,
-	            width: 600,
-	            destroyOnClose: false,
-	            center: false
-	        };
-	        _this.onClick = function (e) {
-	            _this.setState({
-	                mousePosition: {
-	                    x: e.pageX,
-	                    y: e.pageY
-	                },
-	                visible: true
-	            });
-	        };
-	        _this.onClose = function (e) {
-	            // console.log(e);
-	            _this.setState({
-	                visible: false
-	            });
-	        };
-	        _this.onDestroyOnCloseChange = function (e) {
-	            _this.setState({
-	                destroyOnClose: e.target.checked
-	            });
-	        };
-	        _this.changeWidth = function () {
-	            _this.setState({
-	                width: _this.state.width === 600 ? 800 : 600
-	            });
-	        };
-	        _this.center = function (e) {
-	            _this.setState({
-	                center: e.target.checked
-	            });
-	        };
-	        return _this;
-	    }
-	
-	    MyControl.prototype.render = function render() {
-	        var dialog = void 0;
-	        if (this.state.visible || !this.state.destroyOnClose) {
-	            var style = {
-	                width: this.state.width
-	            };
-	            var wrapClassName = '';
-	            if (this.state.center) {
-	                wrapClassName = 'center';
-	            }
-	            dialog = _react2.default.createElement(_DialogWrap2.default, { visible: this.state.visible, wrapClassName: wrapClassName, animation: "zoom", maskAnimation: "fade", onClose: this.onClose, style: style, mousePosition: this.state.mousePosition }, _react2.default.createElement("input", null), _react2.default.createElement("p", null, "basic modal"), _react2.default.createElement("button", { onClick: this.changeWidth }, "change width"), _react2.default.createElement("div", { style: { height: 200 } }));
-	        }
-	        return _react2.default.createElement("div", { style: { width: '90%', margin: '0 auto' } }, _react2.default.createElement("style", null, '\n            .center {\n              display: flex;\n              align-items: center;\n              justify-content: center;\n            }\n            '), _react2.default.createElement("p", null, _react2.default.createElement("button", { className: "btn btn-primary", onClick: this.onClick }, "show dialog"), '\xA0', _react2.default.createElement("label", null, "destroy on close:", _react2.default.createElement("input", { type: "checkbox", checked: this.state.destroyOnClose, onChange: this.onDestroyOnCloseChange })), '\xA0', _react2.default.createElement("label", null, "center", _react2.default.createElement("input", { type: "checkbox", checked: this.state.center, onChange: this.center }))), dialog);
-	    };
-	
-	    return MyControl;
-	}(_react2.default.Component);
-	// use import Dialog from 'rc-dialog'
-	
-	
-	_reactDom2.default.render(_react2.default.createElement("div", null, _react2.default.createElement("h2", null, "ant-design dialog"), _react2.default.createElement(MyControl, null)), document.getElementById('__react-content'));
+/* eslint no-console:0 */
+
+
+
+// use import Dialog from 'rc-dialog'
+
+
+var MyControl = function (_React$Component) {
+    __WEBPACK_IMPORTED_MODULE_3_babel_runtime_helpers_inherits___default()(MyControl, _React$Component);
+
+    function MyControl() {
+        __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck___default()(this, MyControl);
+
+        var _this = __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_possibleConstructorReturn___default()(this, (MyControl.__proto__ || Object.getPrototypeOf(MyControl)).apply(this, arguments));
+
+        _this.state = {
+            visible: false,
+            width: 600,
+            destroyOnClose: false,
+            center: false
+        };
+        _this.onClick = function (e) {
+            _this.setState({
+                mousePosition: {
+                    x: e.pageX,
+                    y: e.pageY
+                },
+                visible: true
+            });
+        };
+        _this.onClose = function (e) {
+            // console.log(e);
+            _this.setState({
+                visible: false
+            });
+        };
+        _this.onDestroyOnCloseChange = function (e) {
+            _this.setState({
+                destroyOnClose: e.target.checked
+            });
+        };
+        _this.changeWidth = function () {
+            _this.setState({
+                width: _this.state.width === 600 ? 800 : 600
+            });
+        };
+        _this.center = function (e) {
+            _this.setState({
+                center: e.target.checked
+            });
+        };
+        return _this;
+    }
+
+    __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_createClass___default()(MyControl, [{
+        key: 'render',
+        value: function render() {
+            var dialog = void 0;
+            if (this.state.visible || !this.state.destroyOnClose) {
+                var style = {
+                    width: this.state.width
+                };
+                var wrapClassName = '';
+                if (this.state.center) {
+                    wrapClassName = 'center';
+                }
+                dialog = __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_7__src_DialogWrap__["a" /* default */], { visible: this.state.visible, wrapClassName: wrapClassName, animation: "zoom", maskAnimation: "fade", onClose: this.onClose, style: style, mousePosition: this.state.mousePosition }, __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("input", null), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("p", null, "basic modal"), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("button", { onClick: this.changeWidth }, "change width"), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("div", { style: { height: 200 } }));
+            }
+            return __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("div", { style: { width: '90%', margin: '0 auto' } }, __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("style", null, '\n            .center {\n              display: flex;\n              align-items: center;\n              justify-content: center;\n            }\n            '), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("p", null, __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("button", { className: "btn btn-primary", onClick: this.onClick }, "show dialog"), '\xA0', __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("label", null, "destroy on close:", __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("input", { type: "checkbox", checked: this.state.destroyOnClose, onChange: this.onDestroyOnCloseChange })), '\xA0', __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("label", null, "center", __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("input", { type: "checkbox", checked: this.state.center, onChange: this.center }))), dialog);
+        }
+    }]);
+
+    return MyControl;
+}(__WEBPACK_IMPORTED_MODULE_5_react___default.a.Component);
+
+__WEBPACK_IMPORTED_MODULE_6_react_dom___default.a.render(__WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("div", null, __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("h2", null, "ant-design dialog"), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(MyControl, null)), document.getElementById('__react-content'));
 
 /***/ }),
 
-/***/ 80:
+/***/ 185:
 /***/ (function(module, exports) {
 
-	// removed by extract-text-webpack-plugin
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 292:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(132);
+
 
 /***/ })
 
-});
+},[292]);
 //# sourceMappingURL=ant-design.js.map
--- examples/bootstrap.js
+++ examples/bootstrap.js
@@ -1,102 +1,106 @@
-webpackJsonp([1],{
+webpackJsonp([0],{
 
-/***/ 0:
-/***/ (function(module, exports, __webpack_require__) {
+/***/ 133:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-	module.exports = __webpack_require__(281);
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck__ = __webpack_require__(40);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_createClass__ = __webpack_require__(41);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_createClass___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_createClass__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_possibleConstructorReturn__ = __webpack_require__(43);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_possibleConstructorReturn___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_possibleConstructorReturn__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_babel_runtime_helpers_inherits__ = __webpack_require__(42);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_babel_runtime_helpers_inherits___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_babel_runtime_helpers_inherits__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_bootstrap_dist_css_bootstrap_css__ = __webpack_require__(183);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_bootstrap_dist_css_bootstrap_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_bootstrap_dist_css_bootstrap_css__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_rc_dialog_assets_bootstrap_less__ = __webpack_require__(184);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_rc_dialog_assets_bootstrap_less___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5_rc_dialog_assets_bootstrap_less__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_react__ = __webpack_require__(13);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_6_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7_react_dom__ = __webpack_require__(35);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_7_react_dom__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__src_DialogWrap__ = __webpack_require__(86);
 
 
-/***/ }),
 
-/***/ 281:
-/***/ (function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
-	var _classCallCheck2 = __webpack_require__(2);
-	
-	var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
-	
-	var _possibleConstructorReturn2 = __webpack_require__(3);
-	
-	var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
-	
-	var _inherits2 = __webpack_require__(72);
-	
-	var _inherits3 = _interopRequireDefault(_inherits2);
-	
-	__webpack_require__(282);
-	
-	__webpack_require__(283);
-	
-	var _react = __webpack_require__(81);
-	
-	var _react2 = _interopRequireDefault(_react);
-	
-	var _reactDom = __webpack_require__(116);
-	
-	var _reactDom2 = _interopRequireDefault(_reactDom);
-	
-	var _DialogWrap = __webpack_require__(262);
-	
-	var _DialogWrap2 = _interopRequireDefault(_DialogWrap);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	var MyControl = function (_React$Component) {
-	    (0, _inherits3.default)(MyControl, _React$Component);
-	
-	    function MyControl() {
-	        (0, _classCallCheck3.default)(this, MyControl);
-	
-	        var _this = (0, _possibleConstructorReturn3.default)(this, _React$Component.apply(this, arguments));
-	
-	        _this.state = {
-	            visible: false,
-	            destroyOnClose: false
-	        };
-	        _this.onClick = function () {
-	            _this.setState({
-	                visible: true
-	            });
-	        };
-	        _this.onClose = function () {
-	            _this.setState({
-	                visible: false
-	            });
-	        };
-	        _this.onDestroyOnCloseChange = function (e) {
-	            _this.setState({
-	                destroyOnClose: e.target.checked
-	            });
-	        };
-	        return _this;
-	    }
-	
-	    MyControl.prototype.render = function render() {
-	        var dialog = void 0;
-	        if (this.state.visible || !this.state.destroyOnClose) {
-	            dialog = _react2.default.createElement(_DialogWrap2.default, { visible: this.state.visible, animation: "slide-fade", maskAnimation: "fade", onClose: this.onClose, style: { width: 600 }, title: _react2.default.createElement("div", null, '\u7B2C\u4E8C\u4E2A\u5F39\u6846'), footer: [_react2.default.createElement("button", { type: "button", className: "btn btn-default", key: "close", onClick: this.onClose }, "Close"), _react2.default.createElement("button", { type: "button", className: "btn btn-primary", key: "save", onClick: this.onClose }, "Save changes")] }, _react2.default.createElement("h4", null, "Text in a modal"), _react2.default.createElement("p", null, "Duis mollis, est non commodo luctus, nisi erat porttitor ligula."), _react2.default.createElement("hr", null), _react2.default.createElement("h4", null, "Overflowing text to show scroll behavior"), _react2.default.createElement("p", null, "Cras mattis consectetur purus sit amet fermentum." + " " + "Cras justo odio, dapibus ac facilisis in," + " " + "egestas eget quam. Morbi leo risus, porta ac consectetur ac," + " " + "vestibulum at eros."), _react2.default.createElement("p", null, "Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus" + " " + "sagittis lacus vel augue laoreet rutrum faucibus dolor auctor."), _react2.default.createElement("div", { style: { display: '' } }, _react2.default.createElement("p", null, "Aenean lacinia bibendum nulla sed" + " " + "consectetur. Praesent commodo cursus magna," + " " + "vel scelerisque nisl consectetur et. Donec sed odio dui." + " " + "Donec" + " " + "ullamcorper nulla non metus auctor fringilla."), _react2.default.createElement("p", null, "Cras mattis consectetur purus sit amet fermentum." + " " + "Cras justo odio, dapibus ac facilisis in, egestas" + " " + "eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros."), _react2.default.createElement("p", null, "Praesent commodo cursus" + " " + "magna, vel scelerisque nisl consectetur et." + " " + "Vivamus sagittis lacus vel augue laoreet rutrum faucibus" + " " + "dolor auctor."), _react2.default.createElement("p", null, "Aenean lacinia bibendum nulla sed consectetur." + " " + "Praesent commodo cursus magna, vel" + " " + "scelerisque nisl consectetur et. Donec sed odio dui." + " " + "Donec ullamcorper nulla non metus auctor" + " " + "fringilla."), _react2.default.createElement("p", null, "Cras mattis consectetur purus sit amet fermentum." + " " + "Cras justo odio, dapibus ac" + " " + "facilisis in, egestas eget quam. Morbi leo risus," + " " + "porta ac consectetur ac, vestibulum at eros."), _react2.default.createElement("p", null, "Praesent commodo cursus magna, vel scelerisque nisl consectetur et." + " " + "Vivamus sagittis lacus vel augue" + " " + "laoreet rutrum faucibus dolor auctor."), _react2.default.createElement("p", null, "Aenean lacinia bibendum nulla sed consectetur. Praesent" + " " + "commodo cursus magna, vel scelerisque nisl consectetur et." + " " + "Donec sed odio dui. Donec ullamcorper nulla" + " " + "non metus auctor fringilla.")));
-	        }
-	        return _react2.default.createElement("div", { style: { margin: 20 } }, _react2.default.createElement("p", null, _react2.default.createElement("button", { className: "btn btn-primary", onClick: this.onClick }, "show dialog"), '\xA0', _react2.default.createElement("label", null, "destroy on close:", _react2.default.createElement("input", { type: "checkbox", checked: this.state.destroyOnClose, onChange: this.onDestroyOnCloseChange }))), dialog);
-	    };
-	
-	    return MyControl;
-	}(_react2.default.Component);
-	
-	_reactDom2.default.render(_react2.default.createElement("div", null, _react2.default.createElement("h2", null, "ant-design dialog"), _react2.default.createElement(MyControl, null)), document.getElementById('__react-content'));
+
+
+
+
+
+
+var MyControl = function (_React$Component) {
+    __WEBPACK_IMPORTED_MODULE_3_babel_runtime_helpers_inherits___default()(MyControl, _React$Component);
+
+    function MyControl() {
+        __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck___default()(this, MyControl);
+
+        var _this = __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_possibleConstructorReturn___default()(this, (MyControl.__proto__ || Object.getPrototypeOf(MyControl)).apply(this, arguments));
+
+        _this.state = {
+            visible: false,
+            destroyOnClose: false
+        };
+        _this.onClick = function () {
+            _this.setState({
+                visible: true
+            });
+        };
+        _this.onClose = function () {
+            _this.setState({
+                visible: false
+            });
+        };
+        _this.onDestroyOnCloseChange = function (e) {
+            _this.setState({
+                destroyOnClose: e.target.checked
+            });
+        };
+        return _this;
+    }
+
+    __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_createClass___default()(MyControl, [{
+        key: 'render',
+        value: function render() {
+            var dialog = void 0;
+            if (this.state.visible || !this.state.destroyOnClose) {
+                dialog = __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_8__src_DialogWrap__["a" /* default */], { visible: this.state.visible, animation: "slide-fade", maskAnimation: "fade", onClose: this.onClose, style: { width: 600 }, title: __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("div", null, '\u7B2C\u4E8C\u4E2A\u5F39\u6846'), footer: [__WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("button", { type: "button", className: "btn btn-default", key: "close", onClick: this.onClose }, "Close"), __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("button", { type: "button", className: "btn btn-primary", key: "save", onClick: this.onClose }, "Save changes")] }, __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("h4", null, "Text in a modal"), __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("p", null, "Duis mollis, est non commodo luctus, nisi erat porttitor ligula."), __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("hr", null), __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("h4", null, "Overflowing text to show scroll behavior"), __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("p", null, "Cras mattis consectetur purus sit amet fermentum." + " " + "Cras justo odio, dapibus ac facilisis in," + " " + "egestas eget quam. Morbi leo risus, porta ac consectetur ac," + " " + "vestibulum at eros."), __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("p", null, "Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus" + " " + "sagittis lacus vel augue laoreet rutrum faucibus dolor auctor."), __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("div", { style: { display: '' } }, __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("p", null, "Aenean lacinia bibendum nulla sed" + " " + "consectetur. Praesent commodo cursus magna," + " " + "vel scelerisque nisl consectetur et. Donec sed odio dui." + " " + "Donec" + " " + "ullamcorper nulla non metus auctor fringilla."), __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("p", null, "Cras mattis consectetur purus sit amet fermentum." + " " + "Cras justo odio, dapibus ac facilisis in, egestas" + " " + "eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros."), __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("p", null, "Praesent commodo cursus" + " " + "magna, vel scelerisque nisl consectetur et." + " " + "Vivamus sagittis lacus vel augue laoreet rutrum faucibus" + " " + "dolor auctor."), __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("p", null, "Aenean lacinia bibendum nulla sed consectetur." + " " + "Praesent commodo cursus magna, vel" + " " + "scelerisque nisl consectetur et. Donec sed odio dui." + " " + "Donec ullamcorper nulla non metus auctor" + " " + "fringilla."), __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("p", null, "Cras mattis consectetur purus sit amet fermentum." + " " + "Cras justo odio, dapibus ac" + " " + "facilisis in, egestas eget quam. Morbi leo risus," + " " + "porta ac consectetur ac, vestibulum at eros."), __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("p", null, "Praesent commodo cursus magna, vel scelerisque nisl consectetur et." + " " + "Vivamus sagittis lacus vel augue" + " " + "laoreet rutrum faucibus dolor auctor."), __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("p", null, "Aenean lacinia bibendum nulla sed consectetur. Praesent" + " " + "commodo cursus magna, vel scelerisque nisl consectetur et." + " " + "Donec sed odio dui. Donec ullamcorper nulla" + " " + "non metus auctor fringilla.")));
+            }
+            return __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("div", { style: { margin: 20 } }, __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("p", null, __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("button", { className: "btn btn-primary", onClick: this.onClick }, "show dialog"), '\xA0', __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("label", null, "destroy on close:", __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("input", { type: "checkbox", checked: this.state.destroyOnClose, onChange: this.onDestroyOnCloseChange }))), dialog);
+        }
+    }]);
+
+    return MyControl;
+}(__WEBPACK_IMPORTED_MODULE_6_react___default.a.Component);
+
+__WEBPACK_IMPORTED_MODULE_7_react_dom___default.a.render(__WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("div", null, __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement("h2", null, "ant-design dialog"), __WEBPACK_IMPORTED_MODULE_6_react___default.a.createElement(MyControl, null)), document.getElementById('__react-content'));
 
 /***/ }),
 
-/***/ 282:
+/***/ 183:
 /***/ (function(module, exports) {
 
-	// removed by extract-text-webpack-plugin
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
-/***/ 283:
-282
+/***/ 184:
+/***/ (function(module, exports) {
 
-});
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 293:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(133);
+
+
+/***/ })
+
+},[293]);
 //# sourceMappingURL=bootstrap.js.map
