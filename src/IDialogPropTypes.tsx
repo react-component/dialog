fbc827d4f9e8b88fe9127dc45598ce5190f1d6c8
zombiej <smith3816@gmail.com>
2020-09-28 14:00:19 +0800

    fix ts

--- src/IDialogPropTypes.tsx
+++ src/IDialogPropTypes.tsx
@@ -1,6 +1,5 @@
 import { ReactNode, CSSProperties, SyntheticEvent } from 'react';
-
-type IStringOrHtmlElement = string | HTMLElement;
+import { GetContainer } from 'rc-util/lib/PortalWrapper';
 
 export interface IDialogPropTypes {
   className?: string;
@@ -13,10 +12,10 @@ export interface IDialogPropTypes {
   closable?: boolean;
   maskClosable?: boolean;
   visible?: boolean;
-  destroyOnClose ?: boolean;
+  destroyOnClose?: boolean;
   mousePosition?: {
-    x: number,
-    y: number,
+    x: number;
+    y: number;
   };
   title?: ReactNode;
   footer?: ReactNode;
@@ -35,7 +34,7 @@ export interface IDialogPropTypes {
   bodyProps?: any;
   maskProps?: any;
   wrapProps?: any;
-  getContainer?: IStringOrHtmlElement | (() => IStringOrHtmlElement) | false;
+  getContainer?: GetContainer | false;
   closeIcon?: ReactNode;
   modalRender?: (node: ReactNode) => ReactNode;
   forceRender?: boolean;
